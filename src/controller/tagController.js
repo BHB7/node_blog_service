47e22c03b26c4f33a5d43a11f296cf3661c15976
BHB7 <1812287263@qq.com>
2025-04-27 15:53:50 +0800

    req allType

--- src/controller/tagController.js
+++ src/controller/tagController.js
@@ -1,8 +1,8 @@
 const { getTagService, createTagService, deleteTagService } = require('../service/tagService');
 
 const getTagController = async (req, res) => {
+  const reqAllType = req.body || req.query;
   try {
-
     const data = await getTagService(req);
     res.success(data);
   } catch (error) {
@@ -10,7 +10,7 @@ const getTagController = async (req, res) => {
   };
 };
 const createTagController = async (req, res) => {
-  const reqAllType = req.body || req.query || req.params;
+  const reqAllType = req.body || req.query;
   if (!reqAllType.name) return res.error('哎哟，参数不太乖，得再检查一下哦~');
   try {
     const data = await createTagService(reqAllType);
@@ -20,13 +20,13 @@ const createTagController = async (req, res) => {
   };
 };
 const deleteTagController = async (req, res) => {
-  const { tid } = req.body || req.query;
-  if (!tid) return res.error('参数不合法 ');
+  const reqAllType = req.body || req.query;
+  if (!reqAllType.tid) return res.error('参数不合法 ');
   try {
-    await deleteTagService(req);
+    await deleteTagService(reqAllType);
   } catch (error) {
     res.error('删除标签失败啦');
-  }
+  };
 };
 
 module.exports = {
