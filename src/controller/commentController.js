b758e4605a77ca95e7922f5faf84edde375d7f93
BHB7 <1812287263@qq.com>
2025-05-19 18:06:51 +0800

    ....

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -5,7 +5,7 @@ const { addCommentService, delCommentService, getCommentsService } = require('..
 const addCommentController = async (req, res) => {
      // 从 JWT 中获取用户 ID
     const user_id = req.auth?.id || req.user?.id; // 兼容 req.auth 和 req.user
-    const { content, aid, uid, pid } = req.body;
+    let { content, aid, uid, pid } = req.body;
     if(!uid) uid = user_id;
     try {
         const response = await addCommentService({ content, aid, uid, pid });
